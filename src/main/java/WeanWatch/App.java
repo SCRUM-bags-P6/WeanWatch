0f472266833507c43c40ebee6e2c59b36bd897bb
lleuch18 <61183192+lleuch18@users.noreply.github.com>
2021-05-06 10:49:44 +0200

    Metaphorical figure 1. iteration

--- src/main/java/WeanWatch/App.java
+++ src/main/java/WeanWatch/App.java
@@ -7,6 +7,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import WeanWatch.controller.RootCtrl;
+import WeanWatch.controller.TriangleMetaphoricCtrl;
 import WeanWatch.model.PDMSConn;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -28,7 +29,7 @@ public class App extends Application {
 
     public static void main(String[] args) {
         // Launch JavaFX
-        //launch(args); 
+        launch(args); 
 
         PDMSConn pdmsConn = PDMSConn.getInstance();
 
@@ -51,27 +52,13 @@ public class App extends Application {
         loader.setLocation(App.class.getClassLoader().getResource("view/LoginView.fxml"));
         BorderPane view = (BorderPane) loader.load();
         
-        // FXMLLoader loader = new FXMLLoader();
-        // loader.setLocation(App.class.getClassLoader().getResource("view/rootView.fxml"));
-        // BorderPane view = (BorderPane) loader.load();
-
+		//Kun til test af figurer
 		DetectedCase detectedcase = new DetectedCase();
 
 		TriangleMetaphoricCtrl MCtrl = new TriangleMetaphoricCtrl(detectedcase);
 
-		Polygon firstTriangle = MCtrl.drawFigure(0, 300.0, 100.0, 150.0, 200.0, 400.0, 500.0);
-		Polygon secondTriangle = MCtrl.drawFigure(1, 800.0, 600.0, 400.0, 450.0, 900.0, 1000.0);
-		Polygon thirdTriangle = MCtrl.drawFigure(2, 1000.0, 1200.0, 800.0, 900.0, 1300.0, 1400.0);
-
-
-		firstTriangle.setFill(javafx.scene.paint.Color.RED);
-		
-
-		Group root = new Group(firstTriangle, secondTriangle, thirdTriangle);
-	
-		
-
-        
+		//Group root = MCtrl.drawFigure(0, 300.0, 100.0, 150.0, 200.0, 400.0, 500.0, 1, 800.0, 600.0, 400.0, 450.0, 900.0, 1000.0);
+		Group root = MCtrl.drawFigure(0, 960.00, 590.00, 1080.00, 590.00, 1020.00, 800.00, 1, 960.00, 500.00, 1080.00, 500.00, 1020.0, 400.00);
 
 		try {
 			primaryStage.setScene(new Scene(root, width, height));			
@@ -83,7 +70,7 @@ public class App extends Application {
 			e.printStackTrace();
 		}
 
-		System.out.println(firstTriangle.getPoints());
+		//System.out.println(firstTriangle.getPoints());
         // TODO Auto-generated method stub
         
     }
