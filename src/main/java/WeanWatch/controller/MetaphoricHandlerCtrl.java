c448f90d9575662c7bb9131ba20bae4789df3a4f
nikolajjustesen <nikolaj-justesen@hotmail.com>
2021-05-06 13:52:40 +0200

    ændring i metaphoricalHandlerView

--- src/main/java/WeanWatch/controller/MetaphoricHandlerCtrl.java
+++ src/main/java/WeanWatch/controller/MetaphoricHandlerCtrl.java
@@ -1,19 +1,37 @@
 package WeanWatch.controller;
 
+import java.io.FileInputStream;
+
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 public class MetaphoricHandlerCtrl {
 
 @FXML
-public HBox FigureHBox;
+public HBox figureHBox;
 
 @FXML
-public VBox FigureVBox;
+public VBox figureVBox;
+
+/*
+HBox figureHBox = new HBox();
+
+figureHBox.getChildren().addAll(Label = new Label();
 
 
 
 
+figureHBox.setPadding(new Insets(100, 100, 100, 100));
+figureHBox.setSpacing(20);
+
+*/
 }
+
+
