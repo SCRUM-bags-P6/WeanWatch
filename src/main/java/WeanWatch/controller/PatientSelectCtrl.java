05f50adb2e3ad8ae4b21074f03476477c859e35a
ldaug99 <lassekd99@live.dk>
2021-05-06 11:21:05 +0200

    Patients are now displayed using a cell

--- src/main/java/WeanWatch/controller/PatientSelectCtrl.java
+++ src/main/java/WeanWatch/controller/PatientSelectCtrl.java
@@ -9,27 +9,30 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 public class PatientSelectCtrl {
     
     @FXML
-    private ListView<String> patientListView;
+    private ListView<Patient> patientListView;
 
-    
+    private ObservableList<Patient> patientList;
 
     public PatientSelectCtrl() {
-        
-        
+        // Create an observable list for the patients
+        patientList = FXCollections.observableArrayList();
+        // Load patients form the handler and add the patients to the observable list
+        patientList.addAll(PatientHandler.getInstance().getPatients());
     }
 
     @FXML
     private void initialize() {
         try {
-
-            for(Patient patient : PatientHandler.getInstance().getPatients()) {
-                patientListView.getItems().add(patient.getCPR());    
-            }
-
+            // Add the loaded patients to the list view
+            patientListView.setItems(patientList);
+            // Add a cell factory for each cell to create a custome using the controller
+            patientListView.setCellFactory(listView -> new PatientSelectListCell());
         } catch (Exception e) {
             
             //TODO: handle exception
---
+++ src/main/java/WeanWatch/controller/PatientSelectListCell.java
@@ -0,0 +1,61 @@
+package WeanWatch.controller;
+
+import WeanWatch.model.Patient;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+
+public class PatientSelectListCell extends ListCell<Patient> {
+    
+    @FXML
+    private Label cprLabel;
+    @FXML
+    private Label ageLabel;
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private HBox cellBox;
+
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    protected void updateItem(Patient patient, boolean empty) {
+        // Call super constructor
+        super.updateItem(patient, empty);
+
+        if (empty || patient == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            if (fxmlLoader == null) {
+                // Load the FXML resource
+                fxmlLoader = new FXMLLoader(getClass().getResource("/view/PatientSelectListCell.fxml"));
+                // Set this instance as the controller
+                fxmlLoader.setController(this);
+                // Try loading the view
+                try {
+                    fxmlLoader.load();
+                } catch (Exception e) {
+
+                    //TODO: handle exception
+                    System.err.println("Failed to load list view cells with error:" + e);
+                    e.printStackTrace();
+
+                }
+            }
+            // Set the label text
+            cprLabel.setText(patient.getCPR());
+            ageLabel.setText(String.valueOf(patient.getAge()));
+            nameLabel.setText(patient.getName());
+
+            System.out.println(patient.getCPR());
+
+            setText(null);
+            setGraphic(cellBox);
+        }
+    }
+
+}
