54585924f44a3b7d2a5010696c7740d49eb96b80
nikolajjustesen <nikolaj-justesen@hotmail.com>
2021-05-06 10:46:01 +0200

    ScreeningView, MetaphoricHandlerView, MetaphoricCaptionView

---
+++ src/main/java/WeanWatch/controller/MetaphoricCaptionCtrl.java
@@ -0,0 +1,24 @@
+package WeanWatch.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class MetaphoricCaptionCtrl {
+
+@FXML
+public GridPane metaphoricCaptionGridPane;
+
+@FXML
+public Label lastOccurrenceLabel;
+    
+@FXML
+public Label numberOfOccurrencesLabel;
+    
+@FXML
+public Label caseNameLabel;
+
+@FXML
+public Label timeLabel;
+
+}
