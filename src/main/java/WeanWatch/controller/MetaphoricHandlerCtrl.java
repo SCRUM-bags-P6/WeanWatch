a58dc9417603d9b167f8e667adc68e31166be57e
nikohecht1 <61183172+nikohecht1@users.noreply.github.com>
2021-05-13 11:10:23 +0200

    Prototyping metaphorical handler

--- src/main/java/WeanWatch/controller/MetaphoricHandlerCtrl.java
+++ src/main/java/WeanWatch/controller/MetaphoricHandlerCtrl.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+
+import java.util.ArrayList;
 
 import WeanWatch.controller.*;
 
@@ -28,7 +31,30 @@ private HBox FigureHBoxBottom;
 
 private TriangleMetaphoricCtrl metaphoricCtrl;
 
+public MetaphoricCaptionCtrl() {
+	
+}
 
+
+public void handleFigureClick() {
+
+}
+
+
+
+public void dispFigures(ArrayList<DetectedCase> casesToDisplay, ArrayList<MetaphoricCaptionCtrl> captionsToDisplay) {
+	for (int i = 0; i < casesToDisplay.size(); i++) { //tæl hvor mange cases vi har der skal displayes 
+		// Find ud af om der er plads i HBox
+			// Er i % 3
+				// Hvis ja, lav ny HBox
+				// Hvis nej, indsæt i den nuværende HBox
+		if (i % 3 == 0) {
+			HBox hbox = new HBox ();
+		}
+		hbox.getChildren().add(metaphoricFigure);
+			
+	}
+}
 
 
 
@@ -36,21 +62,24 @@ private TriangleMetaphoricCtrl metaphoricCtrl;
  * Sætter den metaforiske figur i øverste HBox
  */
 public void insertMetaphoricFigureTop(Group metaphoricFigure){
-BorderPane pane = new BorderPane();
-pane.getChildren().add(metaphoricFigure);
+	BorderPane pane = new BorderPane();
+	pane.getChildren().add(metaphoricFigure);
 
-/*
-DEN HER DEL HØRER TIL "DET GAMLE" KODE, HVOR DER BLEV HARDCODED EN TOP OG EN BUND
-this.FigureHBoxTop.setSpacing(700.0);
-this.FigureHBoxTop.getChildren().add(pane);
-*/
+	/*
+	DEN HER DEL HØRER TIL "DET GAMLE" KODE, HVOR DER BLEV HARDCODED EN TOP OG EN BUND
+	this.FigureHBoxTop.setSpacing(700.0);
+	this.FigureHBoxTop.getChildren().add(pane);
+	*/
 
 
-//DEN HER DEL ER TIL DEN NYE KODE, HVOR DER DYNAMISK SKABES HBOXe. PROBLEMET ER LIGENU, AT DEN LAVER EN HBOX FOR HVER FIGUR DEN SÆTTER IND
-//OG DET SKAL I STEDET VÆRE SÅDAN, AT DEN SÆTTER 3 FIGURER i 1 HBOX, OG SÅ 3 FIGURER I EN NY HBOX OSV.
-HBox HBox = createHBox(metaphoricFigure);
+	//DEN HER DEL ER TIL DEN NYE KODE, HVOR DER DYNAMISK SKABES HBOXe. PROBLEMET ER LIGENU, AT DEN LAVER EN HBOX FOR HVER FIGUR DEN SÆTTER IND
+	//OG DET SKAL I STEDET VÆRE SÅDAN, AT DEN SÆTTER 3 FIGURER i 1 HBOX, OG SÅ 3 FIGURER I EN NY HBOX OSV.
+	//PRøv at lave counter der tæller antallet af metaphoricFigures der bliver lavet
+	//Udfra counteren kan der laves if eller for loop, der laver ny HBox for hver tredje figur der bliver lavet
 
-this.FigureVBox.getChildren().add(HBox);
+	HBox HBox = createHBox(metaphoricFigure);
+
+	this.FigureVBox.getChildren().add(HBox);
 
 }
 
@@ -72,13 +101,12 @@ public void setMetaphoricCtrl(TriangleMetaphoricCtrl metaphoricCtrl){
 
 
 public HBox createHBox (Group metaphoricFigure){
-HBox HBox = new HBox();
+	HBox HBox = new HBox();
 
-HBox.setSpacing(700.0);
-HBox.getChildren().add(metaphoricFigure);
+	HBox.setSpacing(700.0);
+	HBox.getChildren().add(metaphoricFigure);
 
-return HBox;
-
+	return HBox;
 }
 
 }
